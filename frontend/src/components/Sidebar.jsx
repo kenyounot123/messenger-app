3a451d3c356b8b7b6e38f4aa32193d184de0d5a8
kenyounot123 <kenlu519@gmail.com>
2024-06-26 09:00:02 -0400

    Update css for responsive design

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,14 +2,14 @@ import { SettingsIcon, ChatIcon, Icon } from "@chakra-ui/icons";
 import { CgProfile } from "react-icons/cg";
 export default function Sidebar() {
   return (
-    <div className="flex flex-col">
-      <div className="text-center">
-        {" "}
-        <SettingsIcon boxSize={30} />{" "}
-      </div>
+    <div className="flex lg:flex-col gap-10">
       <div className="text-center">
         {" "}
         <ChatIcon boxSize={30} />{" "}
+      </div>
+      <div className="text-center">
+        {" "}
+        <SettingsIcon boxSize={30} />{" "}
       </div>
       <div className="text-center">
         {" "}
--- frontend/src/routes/Homepage/Homepage.jsx
+++ frontend/src/routes/Homepage/Homepage.jsx
@@ -3,15 +3,17 @@ import Sidebar from "../../components/Sidebar";
 export default function Homepage() {
   return (
     <div className="py-5 flex justify-center items-center h-screen w-screen bg-gradient-to-b from-gradient-black to-gradient-grey">
-      <div className="gap-x-5 gap-y-3 grid grid-cols-12 grid-rows-12 w-11/12 bg-gradient-to-b from-gradient-box-grey to-gradient-box-white self-center bg-black rounded-xl min-h-full">
-        <div className="ml-5 row-span-12 my-5 rounded-xl bg-messenger-blue">
+      <div className="p-5 gap-x-5 gap-y-3 grid grid-cols-12 grid-rows-12 w-11/12 bg-gradient-to-b from-gradient-box-grey to-gradient-box-white self-center min-h-96 max-h-full bg-black rounded-xl">
+        <div className="flex justify-center items-center rounded-xl row-start-11 col-start-1 col-span-12 row-span-3 md:row-start-11 md:row-span-2 md:col-start-1 md:col-span-12 lg:col-start-1 lg:col-span-1 lg:row-span-12 bg-messenger-blue">
           <Sidebar></Sidebar>
         </div>
-        <div className="col-span-3 mt-5 rounded-xl bg-white">2</div>
-        <div className="row-span-11 col-start-2 col-span-3 mb-5 rounded-xl bg-white">
+        <div className="col-span-12 row-span-2 md:col-start-1 md:row-span-2 md:col-span-3 lg:col-start-2 lg:col-span-3 rounded-xl bg-white">
+          2
+        </div>
+        <div className="col-span-12 row-span-2 md:col-start-1 md:row-span-8 md:col-span-3 lg:col-start-2 lg:col-span-3 lg:row-span-10 rounded-xl bg-white">
           3
         </div>
-        <div className="rounded-xl row-span-12 col-span-8 mr-5 row-start-1 col-start-5 my-5 bg-white">
+        <div className="col-span-12 row-span-6 rounded-xl md:row-span-10 md:col-span-9 md:row-start-1 md:col-start-4 lg:col-start-5 lg:row-span-12 lg:row-start-1 bg-white">
           4
         </div>
       </div>
