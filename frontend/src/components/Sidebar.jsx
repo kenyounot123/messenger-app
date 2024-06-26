1a50cc82c71b9b05532244530f758413bfa84131
kenyounot123 <kenlu519@gmail.com>
2024-06-25 23:27:01 -0400

    Use chakra ui and react icons

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,10 +1,20 @@
+import { SettingsIcon, ChatIcon, Icon } from "@chakra-ui/icons";
+import { CgProfile } from "react-icons/cg";
 export default function Sidebar() {
   return (
     <div className="flex flex-col">
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
+      <div className="text-center">
+        {" "}
+        <SettingsIcon boxSize={30} />{" "}
+      </div>
+      <div className="text-center">
+        {" "}
+        <ChatIcon boxSize={30} />{" "}
+      </div>
+      <div className="text-center">
+        {" "}
+        <Icon as={CgProfile} boxSize={30} />{" "}
+      </div>
     </div>
   );
 }
--- frontend/src/routes/Homepage/Homepage.jsx
+++ frontend/src/routes/Homepage/Homepage.jsx
@@ -8,10 +8,10 @@ export default function Homepage() {
           <Sidebar></Sidebar>
         </div>
         <div className="col-span-3 mt-5 rounded-xl bg-white">2</div>
-        <div className="row-span-11 col-start-2 col-span-3 mb-5 rounded-xl bg-messenger-blue">
+        <div className="row-span-11 col-start-2 col-span-3 mb-5 rounded-xl bg-white">
           3
         </div>
-        <div className="rounded-xl row-span-12 col-span-8 mr-5 row-start-1 col-start-5 my-5 bg-messenger-blue">
+        <div className="rounded-xl row-span-12 col-span-8 mr-5 row-start-1 col-start-5 my-5 bg-white">
           4
         </div>
       </div>
