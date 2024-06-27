362ad9230fbe88ba3d565a1cae7959e045d6cc61
kenyounot123 <kenlu519@gmail.com>
2024-06-27 17:52:18 -0400

    Fix chatroom table name

--- db/migrate/20240627193302_add_chat_room_table.rb
+++ db/migrate/20240627193302_add_chat_room_table.rb
@@ -1,12 +1,12 @@
 
 class AddChatRoomTable < ActiveRecord::Migration[7.1]
   def change
-    create_table :chatrooms do |t|
+    create_table :chat_rooms do |t|
       t.timestamps
     end
 
-    add_reference :chatrooms, :message, null: false, foreign_key: true
-    add_reference :chatrooms, :user, null: false, foreign_key: true
+    add_reference :chat_rooms, :message, null: false, foreign_key: true
+    add_reference :chat_rooms, :user, null: false, foreign_key: true
   end
 end
 
