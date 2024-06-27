9ae972d0755ff756b9e68e52ef03b2906d654bb3
kenyounot123 <kenlu519@gmail.com>
2024-06-27 17:52:18 -0400

    Fix chatroom table name

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_06_27_193302) do
-  create_table "chatrooms", force: :cascade do |t|
+  create_table "chat_rooms", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "message_id", null: false
     t.integer "user_id", null: false
-    t.index ["message_id"], name: "index_chatrooms_on_message_id"
-    t.index ["user_id"], name: "index_chatrooms_on_user_id"
+    t.index ["message_id"], name: "index_chat_rooms_on_message_id"
+    t.index ["user_id"], name: "index_chat_rooms_on_user_id"
   end
 
   create_table "devise_api_tokens", force: :cascade do |t|
@@ -58,8 +58,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_27_193302) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "chatrooms", "messages"
-  add_foreign_key "chatrooms", "users"
+  add_foreign_key "chat_rooms", "messages"
+  add_foreign_key "chat_rooms", "users"
   add_foreign_key "messages", "users", column: "user_received_id"
   add_foreign_key "messages", "users", column: "user_sent_id"
 end
