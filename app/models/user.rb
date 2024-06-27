1c196b032ecbf894f013990593ced674beab4574
kenyounot123 <kenlu519@gmail.com>
2024-06-27 16:43:30 -0400

    Add ChatRooms table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :api
   validates :name, presence: true
   validates :email, presence: true
+  has_many :messages
 end
