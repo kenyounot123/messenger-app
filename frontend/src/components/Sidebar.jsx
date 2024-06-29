76b6eb59b78618cdf39b7163df4e11a6b810f9f3
kenyounot123 <kenlu519@gmail.com>
2024-06-29 10:04:54 -0400

    Add logic to handle icon and button clicks, this will switch the page when user clicks on a certain thing

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,22 +1,46 @@
 import { SettingsIcon, ChatIcon, Icon, Search2Icon } from "@chakra-ui/icons";
-import { CgProfile } from "react-icons/cg";
 import UserAvatar from "./UserAvatar";
-export default function Sidebar() {
+export default function Sidebar({ setClickedPage }) {
+  function handleIconClick(e) {
+    const button = e.target.closest("button");
+    const icon = button ? button.id : null;
+    if (icon === "chatIcon") {
+      setClickedPage("messages");
+    } else if (icon === "searchIcon") {
+      setClickedPage("explore");
+    } else if (icon === "settingIcon") {
+      setClickedPage("profile");
+    } else {
+      return;
+    }
+  }
   return (
     <div className="flex items-center lg:flex-col gap-10">
-      <button className="text-center">
+      <button className="text-center p-2">
         {" "}
         <UserAvatar size="md" />
       </button>
-      <button className="text-center">
+      <button
+        id="chatIcon"
+        className="text-center p-2"
+        onClick={(e) => handleIconClick(e)}
+      >
         {" "}
         <ChatIcon color="white" boxSize={30} />{" "}
       </button>
-      <button className="text-center">
+      <button
+        id="searchIcon"
+        className="text-center p-2"
+        onClick={(e) => handleIconClick(e)}
+      >
         {" "}
         <Search2Icon color="white" boxSize={30} />{" "}
       </button>
-      <button className="text-center">
+      <button
+        id="settingIcon"
+        className="text-center p-2"
+        onClick={(e) => handleIconClick(e)}
+      >
         {" "}
         <SettingsIcon color="white" boxSize={30} />{" "}
       </button>
