59f6e0f90c1cd2a29cabff7ee0f130bc774cd19e
kenyounot123 <kenlu519@gmail.com>
2024-06-28 20:24:43 -0400

    Render list of users from rails backend

--- frontend/src/components/ExploreUsersBox.jsx
+++ frontend/src/components/ExploreUsersBox.jsx
@@ -1,17 +1,27 @@
 import { useState, useEffect, useContext } from "react";
 import { UserContext } from "./UserContext";
+import UserAvatar from "./UserAvatar";
 export default function ExploreUsersBox() {
   const { userData, setUserData } = useContext(UserContext);
   return (
-    <div className="m-5 flex flex-col">
-      <div className="flex mb-5 border-b-2 pb-5">
-        {/* user.status and user.name */}
-        <div className="grid grid-rows-2 grow">
-          <p className="font-bold">{userData.current_user.name}</p>
-          <p>Online</p>
+    <>
+      {userData.other_users.map((user) => (
+        <div
+          key={user.id}
+          className="m-5 p-2 text-xs flex gap-5 border-b rounded-md hover:bg-slate-200"
+        >
+          <UserAvatar size={"md"} name={user.name} />
+          <div className="grid grid-cols-2 grow">
+            <h1 className="text-xl text-nowrap font-bold col-span-2">
+              {user.name}{" "}
+            </h1>
+            <p className="text-nowrap col-start-1 col-span-2">
+              Joined on {user.joined_on}
+            </p>
+          </div>
+          <p className="text-nowrap text-end self-center">Click to Chat</p>
         </div>
-        <div className="flex gap-10"></div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 }
