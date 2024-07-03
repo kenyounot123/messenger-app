4b345e8da92f45165f33ee69b61ab6e0b53b2af3
kenyounot123 <kenlu519@gmail.com>
2024-07-03 13:40:15 -0400

    Add index action for chat_rooms controller, this is for component to fetch the chat room on initial load if it exists

--- app/controllers/api/v1/chat_rooms_controller.rb
+++ app/controllers/api/v1/chat_rooms_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::ChatRoomsController < ApplicationController
   def index
+    user_ids = params[:user_ids]
+    binding.break
+    @chat_room = ChatRoom.joins(:users).where(users: { id: user_ids }).group("chat_rooms.id").having("COUNT(users.id) = ?", user_ids.count).first
+    render json: @chat_room
   end
   def create
     user_ids = params[:chat_room][:user_ids]
